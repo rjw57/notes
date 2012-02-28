376a306a13b12c9961a608d1eedb119d9c650612
Rich Wareham <rjw57@cam.ac.uk>
2012-02-28 00:48:47 +0000

    improve git log -> tikz script

--- technical/using-git/gitlog2tikz.sh
+++ technical/using-git/gitlog2tikz.sh
@@ -1,49 +1,5 @@
 #!/bin/sh
 
-# Arguments are: "y-coord" "[|\*]* <commit hash>"
-function tikz_commit_node()
-{
-    # Do nothing with no arguments
-    if [ "$#" == "0" ]; then
-        return
-    fi
-
-    # Get the line
-    _y=$1; _line=$2
-
-    # Get node position string, '|' is a space, '*' is this node
-    _cur_pos=0
-    _x=-1
-    while [[ "$_line" == [\|\*]\ * ]]; do
-        case "$_line" in
-            '* '*)
-                _x=${_cur_pos}
-                _line=${_line#\* }
-                ;;
-
-            '| '*)
-                _line=${_line#\| }
-                ;;
-
-            *)
-                echo "Unknown positioning character at head of line: $_line" >/dev/null
-                return
-        esac
-        # Advance x-position
-        _cur_pos=$(($_cur_pos + 1))
-    done
-    _graph_width=${_cur_pos}
-
-    # Remaining line is commit hash
-    _line_array=(${_line})
-
-    # Get commit and parent hashes
-    _commit=${_line_array[0]}
-
-    # Write node
-    echo "    \\node[draw, circle] at (-${_x}, ${_y}) (${_commit}) {};"
-}
-
 # Check if a value exists in an array
 # @param $1 mixed  Needle  
 # @param $2 array  Haystack
@@ -59,72 +15,87 @@ in_array() {
     return 1
 }
 
-GIT_LOG_ARGS=$@
-MAX_SUBJECT_LENGTH=62
-
-# get git's output, skip lines without a '*' in them
-git_log=`git log ${GIT_LOG_ARGS} --graph "--pretty=format:%h %p : %s" | grep '\*'`
-
 echo '.. tikz::'
 echo '    :libs: calc'
 echo ''
+echo '    [x=12pt, y=12pt, every node/.style={inner sep=0.4ex}, very thick]'
+echo ''
 
-# Output a set of nodes for the commit
-_y=0
-echo '    \begin{scope}[x=1.2em, y=1.2em, every node/.style={inner sep=0.4ex}]'
-echo "${git_log}" | while read line; do
-    tikz_commit_node "${_y}" "${line}"
-    _y=$((${_y} - 1))
-done
+MAX_SUBJECT_LEN=50
 
-# Get a lit of commits in this log
-_commits=(`echo "${git_log}" | sed -e 's/^[\|\* ]*//' | cut -f 1 -d ' '`)
+# Extract the log and parse the entries
+x=0; y=0; commits=(); lanes=(); virtual_commits=();
+git log -n 30 --topo-order --reverse --format=format:C:%h%nP:%p%nS:%s%nEOR%n | while IFD= read -r line; do
+    case "${line}" in
+        'C:'*)
+            commit="${line#C:}"
+            ;;
+        'P:'*)
+            parents=(${line#P:})
+            ;;
+        'S:'*)
+            subject="${line#S:}"
+            ;;
+        'EOR')
+            # Add this commit to the list of commits we've processed
+            commits=("${commits[@]}" "${commit}")
 
-# Output the paths connecting the nodes
-_y=0
-echo '    \begin{scope}[out=90,in=-90,thin]'
-echo "${git_log}" | sed -e 's/^[\|\* ]*//' | while read line; do
-    _line_array=(${line})
+            # Create virtual parents if necessary
+            for parent in ${parents[@]}; do
+                if ! in_array "${parent}" "${commits[@]}"; then
+                    if ! in_array "${parent}" "${virtual_commits[@]}"; then
+                        # Add a virtual parent commit off the end of the diagram and in a new lane
+                        virtual_commits=("${virtual_commits[@]}" "${parent}")
+                        echo "    \\coordinate (${parent}virtual) at (-${#lanes[@]}, -1.5);"
+                        lanes=("${lanes[@]}" "${parent}")
+                        echo "    \\path let \p1 = (${parent}virtual) in coordinate (${parent}) at (\x1, ${y}-1.3);"
+                        echo "    \\draw (${parent}virtual) -- (${parent});"
+                    fi
+                fi
+            done
 
-    _commit=${_line_array[0]}
-    _line_array=(${_line_array[@]:1})
+            # Follow the first parent
+            first_parent="${parents[0]}"
 
-    _parents=()
-    while [[ ${_line_array[0]} == [^:]* ]]; do
-        _parents=("${_parents[@]}" ${_line_array[0]})
-        _line_array=(${_line_array[@]:1})
-    done
+            # Is there a lane for this parent?
+            if in_array "${first_parent}" "${lanes[@]}"; then
+                # Draw the commit node itself aligned to first parent
+                echo "    \\path let \p1 = (${first_parent}) in (\x1, ${y}) node[draw,circle] (${commit}) {};"
 
-    _subject=${_line_array[@]:1}
-    if (( ${#_subject} > ${MAX_SUBJECT_LENGTH}-3 )); then
-        _subject="${_subject:0:${MAX_SUBJECT_LENGTH}-3}..."
-    fi
-    echo "    \\node[anchor=mid west] at (0.5, ${_y}) (${_commit}-label) {\small\texttt{${_commit}}: \verb|${_subject}|};"
-
-    if in_array "${_commit}" "${_commits[@]}"; then
-
-        for _parent in ${_parents[@]}; do
-            if in_array ${_parent} "${_commits[@]}"; then
-                # Calculate relative offset from parent to commit
-                echo "    \\path (\$(${_commit})-(${_parent})\$); \\pgfgetlastxy{\deltax}{\deltay}"
-
-                # Calculate a 'splay' angle to throw commit arrow out at depending on x-offset
-                echo "    \\pgfmathsetmacro{\splayangle}{(\deltax<-1pt)?120:((\deltax>1pt)?60:90))}"
-
-                # Choose whether to splay based on y-separation
-                echo "    \\pgfmathsetmacro{\outangle}{(\deltay > 2em)?\splayangle:90}"
-                echo "    \\draw[out=\outangle] let \p1 = (${_parent}), \p2 = (${_commit}) in"
-                echo "    (${_parent}) to (\x2,\y1+0.8em+0.3*\deltay-0.3*1.2em) -- (${_commit});"
+                # Replace the parent in the lane
+                lanes=(${lanes[@]/${first_parent}/${commit}})
             else
-                echo "    \\draw let \p1 = (${_commit}) in (${_commit}) -- (\x1, 0.5-${#_commits[@]});"
-                echo "    \\draw[dotted] let \p1 = (${_commit}) in (\x1, 0.5-${#_commits[@]}) -- (\x1, -${#_commits[@]});"
+                # Create a new lane for this commit
+                echo "    \\node[draw,circle] (${commit}) at (-${#lanes[@]}, ${y}) {};"
+                lanes=("${lanes[@]}" "${commit}")
             fi
-        done
-    fi
 
-    _y=$((${_y} - 1))
+            if (( ${#subject} > ${MAX_SUBJECT_LEN} )); then
+                subject="${subject:0:$((${MAX_SUBJECT_LEN}-3))}..."
+            fi
+
+            echo "    \\node[anchor=mid west] (${commit}-label) at (${x} + 1, ${y}) {\footnotesize\verb^${commit}: ${subject}^};"
+            echo "    \\draw[color=black!25!white] (${commit}) -- (${commit}-label);"
+
+            # Connect parents
+            for parent in ${parents[@]}; do
+                if in_array "${parent}" "${virtual_commits[@]}"; then
+                    # Calculate relative offset from parent to commit
+                    echo "    \\path (\$(${commit})-(${parent})\$); \\pgfgetlastxy{\deltax}{\deltay}"
+                    echo "    \\pgfmathsetmacro{\absdeltax}{abs(\deltax)}"
+                    echo "    \\draw (${parent}) -- ++(\deltax, \absdeltax pt) -- (${commit});"
+                else
+                    echo "    \\draw (${parent}) -- (${commit});"
+                fi
+            done
+
+            y=$((${y}+1))
+            ;;
+        '')
+            # skip blank lines
+            ;;
+        *)
+            echo "unexpected output from git log: ${line}" >&2
+    esac
 done
-echo '    \end{scope}'
-
-echo '    \end{scope}'
 
