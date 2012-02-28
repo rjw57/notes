c2b3b3a6b0b43bedd0bdd9e02e15302ce78396cf
Rich Wareham <rjw57@cam.ac.uk>
2012-02-28 05:33:25 +0000

    gitlog2tikz.sh: re-write layout algorithm to improve quality of output

--- tools/gitlog2tikz.sh
+++ tools/gitlog2tikz.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Check if a value exists in an array
 # @param $1 mixed  Needle  
@@ -15,13 +15,13 @@ in_array() {
     return 1
 }
 
-echo '\begin{scope}[x=12pt, y=12pt, every node/.style={inner sep=0.4ex}, very thick]'
+echo '\begin{scope}[x=1em, y=1em, every node/.style={inner sep=0.4ex}, very thick]'
 
 MAX_SUBJECT_LEN=50
 
 # Extract the log and parse the entries
-x=0; y=0; commits=(); lanes=(); virtual_commits=();
-git log $@ --topo-order --reverse --format=format:C:%h%nP:%p%nS:%s%nEOR%n | while IFD= read -r line; do
+x=0; y=0; commits=(); lanes=(); connections=();
+while read line; do
     case "${line}" in
         'C:'*)
             commit="${line#C:}"
@@ -33,60 +33,69 @@ git log $@ --topo-order --reverse --format=format:C:%h%nP:%p%nS:%s%nEOR%n | whil
             subject="${line#S:}"
             ;;
         'EOR')
-            # Add this commit to the list of commits we've processed
-            commits=("${commits[@]}" "${commit}")
-
-            # Create virtual parents if necessary
-            for parent in ${parents[@]}; do
-                if ! in_array "${parent}" "${commits[@]}"; then
-                    if ! in_array "${parent}" "${virtual_commits[@]}"; then
-                        # Add a virtual parent commit off the end of the diagram and in a new lane
-                        virtual_commits=("${virtual_commits[@]}" "${parent}")
-                        echo "\\coordinate (${parent}virtual) at (-${#lanes[@]}, -1.5);"
-                        lanes=("${lanes[@]}" "${parent}")
-                        echo "\\path let \p1 = (${parent}virtual) in coordinate (${parent}) at (\x1, ${y}-1);"
-                        echo "\\draw (${parent}virtual) -- (${parent});"
-                    fi
-                fi
-            done
-
-            # Follow the first parent
-            first_parent="${parents[0]}"
-
-            # Is there a lane for this parent?
-            if in_array "${first_parent}" "${lanes[@]}"; then
-                # Draw the commit node itself aligned to first parent
-                echo "\\path let \p1 = (${first_parent}) in (\x1, ${y}) node[draw,circle] (${commit}) {};"
-
-                # Replace the parent in the lane
-                lanes=(${lanes[@]/${first_parent}/${commit}})
-            else
-                # Create a new lane for this commit
-                echo "\\node[draw,circle] (${commit}) at (-${#lanes[@]}, ${y}) {};"
-                lanes=("${lanes[@]}" "${commit}")
-            fi
-
+            # Trim the subject line
             if (( ${#subject} > ${MAX_SUBJECT_LEN} )); then
                 subject="${subject:0:$((${MAX_SUBJECT_LEN}-3))}..."
             fi
 
-            echo "\\node[anchor=mid west] (${commit}-label) at (${x} + 1, ${y})"
-            echo "{\footnotesize\verb^${commit}: ${subject}^};"
-            echo "\\draw[color=black!25!white] (${commit}) -- (${commit}-label);"
+            # Is this the first commit we're recording?
+            if (( ${#lanes} == 0 )); then
+                lanes=(${commit})
+            fi
 
-            # Connect parents
-            for parent in ${parents[@]}; do
-                if in_array "${parent}" "${virtual_commits[@]}"; then
-                    # Calculate relative offset from parent to commit
-                    echo "\\path (\$(${commit})-(${parent})\$); \\pgfgetlastxy{\deltax}{\deltay}"
-                    echo "\\pgfmathsetmacro{\absdeltax}{abs(\deltax)}"
-                    echo "\\draw (${parent}) -- ++(\deltax, \absdeltax pt) -- (${commit});"
-                else
-                    echo "\\draw (${parent}) -- (${commit});"
+            x=0
+            for lane_entry in ${lanes[@]}; do
+                if [ "${lane_entry}" == "${commit}" ]; then
+                    # Draw the commit node itself aligned to first parent
+                    echo "\\path (-${x}, ${y}) node[draw,circle] (${commit}) {};"
+                    echo "\\node[anchor=mid west] (${commit}-label) at (1, ${y})"
+                    echo "{\footnotesize\verb^${commit}: ${subject}^};"
+                    echo "\\draw[color=black!25!white] (${commit}) -- (${commit}-label);"
+
+                    # Add this commit to the list of commits we've processed
+                    commits=("${commits[@]}" "${commit}")
                 fi
+                x=$((${x} + 1))
             done
 
-            y=$((${y}+1))
+            if (( ${#parents[@]} > 0 )); then
+                if in_array "${parents[0]}" "${lanes[@]}"; then
+                    lanes=(${lanes[@]/${commit}/blank})
+                else
+                    lanes=(${lanes[@]/${commit}/${parents[0]}})
+                fi
+                connections=("${connections[@]}" "${commit}:${parents[0]}")
+            fi
+
+            echo "% pre: ${lanes[@]}"
+            for parent in ${parents[@]:1}; do
+                # Allocate the remaining parents' lanes
+                if ! in_array "${parent}" "${lanes[@]}"; then
+                    if in_array "blank" "${lanes[@]}"; then
+                        new_lanes=()
+                        for lane_elem in ${lanes[@]}; do
+                            if [ "${lane_elem}" == "blank" ]; then
+                                if ! in_array "${parent}" "${new_lanes[@]}"; then
+                                    new_lanes=("${new_lanes[@]}" "${parent}")
+                                else
+                                    new_lanes=("${new_lanes[@]}" "${lane_elem}")
+                                fi
+                            else
+                                new_lanes=("${new_lanes[@]}" "${lane_elem}")
+                            fi
+                        done
+                        lanes=(${new_lanes[@]})
+                    else
+                        lanes=("${lanes[@]}" "${parent}")
+                    fi
+                fi
+
+                # Record this connection
+                connections=("${connections[@]}" "${commit}:${parent}")
+            done
+            echo "% post: ${lanes[@]}"
+
+            y=$((${y}-1))
             ;;
         '')
             # skip blank lines
@@ -94,6 +103,29 @@ git log $@ --topo-order --reverse --format=format:C:%h%nP:%p%nS:%s%nEOR%n | whil
         *)
             echo "unexpected output from git log: ${line}" >&2
     esac
+done < <( git log $@ --topo-order --format=format:C:%h%nP:%p%nS:%s%nEOR%n )
+
+# Create 'virtual' parent nodes for remaining lanes
+x=0;
+for parent in ${lanes[@]}; do
+    if [ "${parent}" != "blank" ]; then
+        echo "\\coordinate (${parent}) at (-${x}, ${y});"
+    fi
+    x=$((${x}+1))
+done
+
+for connection in ${connections[@]}; do
+    commit=${connection%%:*}
+    parent=${connection##*:}
+    echo "\\path (\$(${parent})-(${commit})\$); \\pgfgetlastxy{\\deltax}{\\deltay}"
+    echo "\\pgfmathsetmacro{\\absdeltax}{abs(\\deltax)}"
+    echo "\\pgfmathsetmacro{\\slopey}{(abs(\\deltay) > abs(\\deltax)) && (\\absdeltax > 0)}"
+    echo "\\pgfmathapproxequalto{\\slopey}{1}"
+    echo "\\ifpgfmathcomparison"
+    echo "\\draw[looseness=0.2,out=-90,in=90] (${commit}) to ++(\\deltax, -\\absdeltax pt) -- (${parent});"
+    echo "\\else"
+    echo "\\draw[looseness=0.4,out=-90,in=90] (${commit}) to (${parent});"
+    echo "\\fi"
 done
 
 echo '\end{scope}'
