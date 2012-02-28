5e442c1cf32afb2c5bbeb37e887c37a0d00f1a7d
Rich Wareham <rjw57@cam.ac.uk>
2012-02-28 03:03:00 +0000

    git plugin: add git-dir option to specify gir repository

--- git.py
+++ git.py
@@ -1,3 +1,4 @@
+from docutils.parsers.rst import directives
 from sphinx.util.compat import Directive
 
 import os
@@ -10,18 +11,27 @@ class GitLogDirective(Directive):
     required_arguments = 0
     optional_arguments = 0
     final_argument_whitespace = True
-    option_spec = {}
+    option_spec = {'git-dir':directives.unchanged}
 
     def run(self):
         git_log_script = os.path.join(os.getcwd(), 'tools', 'gitlog2tikz.sh')
         log_cmd = [git_log_script]
         if self.content:
             log_cmd.extend(shlex.split((u' '.join(self.content)).encode('ascii')))
-        tikz_generator = subprocess.Popen(log_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        env = {}
+        env.update(os.environ)
+
+        git_dir = self.options.get('git-dir', None)
+        if git_dir:
+            env['GIT_DIR'] = git_dir
+        
+        tikz_generator = subprocess.Popen(log_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
         (tikz_data, err) = tikz_generator.communicate()
         if 0 != tikz_generator.returncode:
             raise RuntimeError(err)
 
+        print(err)
         node = tikz.tikz()
         node['caption'] = ''
         node['tikz'] = tikz_data
